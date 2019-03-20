3d1ff7d50aa65e15496ffcfad4fca2f010e336f2
Tomer <tomerkobcherrypick@gmail.com>
2019-03-20 15:32:05 +0200

    fixed bugs on 4.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Bug from './bugs/3';
+import Bug from './bugs/4';
 
 export default class App extends React.Component {
   render() {
