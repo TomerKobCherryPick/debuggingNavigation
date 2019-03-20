16e6d13cb1ee216af6ec7b085053ba0a40e48e4a
Tomer <tomerkobcherrypick@gmail.com>
2019-03-20 15:02:10 +0200

    fixed bugs on 3.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Bug from './bugs/2';
+import Bug from './bugs/3';
 
 export default class App extends React.Component {
   render() {
--- bugs/3.js
+++ bugs/3.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { StyleSheet, Button, Text, View } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import React from "react";
+import { StyleSheet, Button, Text, View } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center"
   },
   label: {
-    textAlign: 'center',
-  },
+    textAlign: "center"
+  }
 });
 
 class HomeScreen extends React.Component {
   static navigationOptions = {
-    headerTitle: 'Home',
+    headerTitle: "Home"
   };
   render() {
     return (
@@ -23,15 +23,15 @@ class HomeScreen extends React.Component {
         <Button
           title="Go to Jane's Contact Screen"
           onPress={() => {
-            this.props.navigation.navigate('ContactScreen', { name: 'Jane' });
+            this.props.navigation.navigate("ContactScreen", { name: "Jane" });
           }}
         />
         <Button
           title="Edit Bob's Contact Screen"
           onPress={() => {
-            this.props.navigation.navigate('ContactScreen', {
-              name: 'Bob',
-              isEditing: true,
+            this.props.navigation.navigate("ContactScreen", {
+              name: "Bob",
+              isEditing: true
             });
           }}
         />
@@ -45,13 +45,29 @@ class HomeScreen extends React.Component {
 /////
 
 class ContactScreen extends React.Component {
-  static navigationOptions = {
-    headerTitle: 'Jane',
-    headerRight: <Button onPress={() => {}} title="Edit" />,
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerTitle: navigation.getParam("name"),
+      headerRight: navigation.getParam("isEditing") ? (
+        <Button
+          onPress={() => {
+            navigation.pop();
+          }}
+          title={"Done"}
+        />
+      ) : (
+        <Button
+          onPress={() => {
+            navigation.setParams({ isEditing: true });
+          }}
+          title={"Edit"}
+        />
+      )
+    };
   };
 
   render() {
-    const isEditing = this.props.navigation.getParam('isEditing', false);
+    const isEditing = this.props.navigation.getParam("isEditing", false);
     const message = isEditing
       ? 'Now editing. Press the upper right "Done" button to go back.'
       : 'Press "Edit" to start editing.';
@@ -65,7 +81,9 @@ class ContactScreen extends React.Component {
 
 const AppNavigator = createStackNavigator({
   HomeScreen,
-  ContactScreen,
+  ContactScreen
 });
 
-export default AppNavigator;
+const AppContainer = createAppContainer(AppNavigator);
+
+export default AppContainer;
